85b25c11cbe5f64607bc2e97c9ba5d202a0eeb4e
Oleksandr Yaremchenko <psyholord@gmail.com>
2022-12-11 03:27:50 +0200

    Added initial project

---
+++ LoeApp/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+using Color = System.Windows.Media.Color;
+
+namespace LoeApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TimeGroupsService timeGroupsService = new TimeGroupsService();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var timeGroup = timeGroupsService.GetTimeGroup(DateTimeOffset.Now);
+
+            var currentElectricityState = timeGroupsService.GetCurrentElectricityStatus(3);
+
+            var nextElectricityState = timeGroupsService.GetNextElectricityStateEnum(currentElectricityState);
+
+            ApplyStateHeaderStyle(currentElectricityState);
+            ApplyNextStateHeaderStyle(nextElectricityState);
+        }
+
+        private class StateStyle
+        {
+            public SolidColorBrush StateBackground { get; set; }
+            public SolidColorBrush StateForeground { get; set; }
+            public string StateText { get; set; }
+        }
+
+        private Dictionary<ElectricityStateEnum, StateStyle> StateStyleList =
+            new()
+            {
+                {
+                    ElectricityStateEnum.No, new StateStyle
+                    {
+                        StateBackground = new SolidColorBrush(Color.FromRgb(127, 132, 135)),
+                        StateForeground = new SolidColorBrush(Color.FromRgb(250, 248, 241)),
+                        StateText = "NO"
+                    }
+                },
+                {
+                    ElectricityStateEnum.Yes, new StateStyle
+                    {
+                        StateBackground = new SolidColorBrush(Color.FromRgb(229, 237, 183)), // rgb(216, 233, 168)
+                        StateForeground = new SolidColorBrush(Color.FromRgb(78, 159, 61)),
+                        StateText = "YES"
+                    }
+                },
+                {
+                    ElectricityStateEnum.Maybe, new StateStyle
+                    {
+                        StateBackground = new SolidColorBrush(Color.FromRgb(250, 240, 175)),
+                        StateForeground = new SolidColorBrush(Color.FromRgb(198, 151, 73)),
+                        StateText = "MAYBE"
+                    }
+                }
+            };
+
+        private void ApplyStateHeaderStyle(ElectricityStateEnum currentElectricityState)
+        {
+            var currentChosenStyle = StateStyleList[currentElectricityState];
+            StateBackground.Background = currentChosenStyle.StateBackground;
+            StateLabel.Foreground = currentChosenStyle.StateForeground;
+            StateLabel.Content = currentChosenStyle.StateText;
+        }
+
+        private void ApplyNextStateHeaderStyle(ElectricityStateEnum currentElectricityState)
+        {
+            var currentChosenStyle = StateStyleList[currentElectricityState];
+            NextStateBackground.Background = currentChosenStyle.StateBackground;
+            NextStateLabel.Foreground = currentChosenStyle.StateForeground;
+            NextStateLabel.Content = currentChosenStyle.StateText;
+        }
+    }
+}
---
+++ LoeApp/TimeGroupsService.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoeApp;
+
+public class TimeGroupsService
+{
+    private List<ElectricityStateEnum> StatesOffsets = 
+        new()
+        {
+            ElectricityStateEnum.No,
+            ElectricityStateEnum.Yes,
+            ElectricityStateEnum.Maybe
+        };
+    private List<int> GroupOffsets = new() { 0, 1, 2 };
+
+    private List<TimeBorders> TimeBordersList = new()
+    {
+        new()
+        {
+            BorderStart = new TimeSpan(1,0,0),
+            BorderEnd = new TimeSpan(5,0,0)
+        },
+        new()
+        {
+            BorderStart = new TimeSpan(5,0,0),
+            BorderEnd = new TimeSpan(9,0,0)
+        },
+        new()
+        {
+            BorderStart = new TimeSpan(9,0,0),
+            BorderEnd = new TimeSpan(13,0,0)
+        },
+        new()
+        {
+            BorderStart = new TimeSpan(13,0,0),
+            BorderEnd = new TimeSpan(17,0,0)
+        },
+        new()
+        {
+            BorderStart = new TimeSpan(17,0,0),
+            BorderEnd = new TimeSpan(21,0,0)
+        },
+        new()
+        {
+            BorderStart = new TimeSpan(21,0,0),
+            BorderEnd = new TimeSpan(25,0,0)
+        }
+    };
+
+    public TimeBorders GetTimeBordersForGroup(int group)
+    {
+        return TimeBordersList[group];
+    }
+
+    public int GetTimeGroup(DateTimeOffset offset)
+    {
+        var hours = offset.Hour;
+
+        if(hours < 1) return 5;
+
+        for (var i = 0; i < TimeBordersList.Count; i++)
+        {
+            if (hours >= TimeBordersList[i].BorderStart.TotalHours
+                && hours <= TimeBordersList[i].BorderEnd.TotalHours)
+                return i;
+        }
+
+        throw new NotImplementedException();
+    }
+
+    private int GetDayOfWeekNumber(DayOfWeek dayOfWeek)
+    {
+        switch (dayOfWeek)
+        {
+            
+            case DayOfWeek.Monday:
+                return 0;
+            case DayOfWeek.Tuesday:
+                return 1;
+            case DayOfWeek.Wednesday:
+                return 2;
+            case DayOfWeek.Thursday:
+                return 3;
+            case DayOfWeek.Friday:
+                return 4;
+            case DayOfWeek.Saturday:
+                return 5;
+            case DayOfWeek.Sunday:
+                return 6;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    public ElectricityStateEnum GetCurrentElectricityStatus(int group)
+    {
+        var currentTime = DateTimeOffset.Now;
+
+        var offSet = GetTimeGroupOffset(0, currentTime);
+        offSet = GetDayOfWeekOffset(offSet, currentTime.DayOfWeek);
+        offSet = GetGroupOffset(offSet, GroupOffsets[group - 1]);
+
+        return (ElectricityStateEnum)offSet;
+    }
+
+    public ElectricityStateEnum GetNextElectricityStateEnum(ElectricityStateEnum currentElectricityState)
+    {
+        var newState = (int)currentElectricityState + 1;
+
+        if (newState > 2)
+            newState -= 3;
+
+        return (ElectricityStateEnum)newState;
+    }
+
+    private int GetTimeGroupOffset(int prevOffset, DateTimeOffset currentTime)
+    {
+        var timeGroup = GetTimeGroup(currentTime);
+
+        var offset = timeGroup;
+
+        if (offset > 2)
+        {
+            var x = offset / 3;
+            offset -= 3 * x;
+        }
+
+        return offset;
+    }
+
+    private int GetDayOfWeekOffset(int prevOffset, DayOfWeek dayOfWeek)
+    {
+        var dayOfWeekUkraine = GetDayOfWeekNumber(dayOfWeek);
+
+        var offset = (int)prevOffset + dayOfWeekUkraine;
+
+        if (offset > 2)
+        {
+            var x = offset / 3;
+            offset -= 3 * x;
+        }
+
+        return offset;
+    }
+
+    private int GetGroupOffset(int prevOffset, int group)
+    {
+        var offset = prevOffset + group;
+
+        if (offset > 2)
+        {
+            var x = offset / 3;
+            offset -= 3 * x;
+        }
+
+        return offset;
+    }
+}
+
+
+
+public enum ElectricityStateEnum
+{
+    No,
+    Yes,
+    Maybe
+}
+
+
+public class TimeBorders
+{
+    public TimeSpan BorderStart { get; set; }
+    public TimeSpan BorderEnd { get; set; }
+}
